80af6730e6739b25ba54669966d4c39e15dfdc61
MedinaKrelani <medina.krelani@studentet.uni-pr.edu>
2020-04-21 20:57:47 +0200

    Fillimi i punes ne pjesen e 'products'

---
+++ ssh-shop-server/src/routes/products.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+
+
+// supozojme ne ket path /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
+
+module.exports = router;
