85b1e5ae9dc1fb754eb23a15e92e93ddc8c14b14
Lavdimi <lavdim.pireva@studentet.uni-pr.edu>
2020-05-04 23:29:19 +0200

    AddressClient completed

--- ssh-shop-client/src/main/java/module-info.java
+++ ssh-shop-client/src/main/java/module-info.java
@@ -6,5 +6,6 @@ module org.fiek {
     opens org.fiek to javafx.fxml;
     opens org.fiek.controllers.primary to javafx.fxml;
     opens org.fiek.controllers.secondary to javafx.fxml;
+    opens org.fiek.controllers.address to javafx.fxml;
     exports org.fiek;
 }
