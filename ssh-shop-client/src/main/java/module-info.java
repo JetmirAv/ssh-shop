e4b50fca6e94aa5d41b7dd835f557c34c80e30f3
Jetmir Avdullahu <jetmir.avdullahu@studentet.uni-pr.edu>
2020-05-05 01:36:00 +0200

    remove unnecessary files

--- ssh-shop-client/src/main/java/module-info.java
+++ ssh-shop-client/src/main/java/module-info.java
@@ -4,7 +4,5 @@ module org.fiek {
     requires com.jfoenix;
 
     opens org.fiek to javafx.fxml;
-    opens org.fiek.controllers.primary to javafx.fxml;
-    opens org.fiek.controllers.secondary to javafx.fxml;
     exports org.fiek;
 }
