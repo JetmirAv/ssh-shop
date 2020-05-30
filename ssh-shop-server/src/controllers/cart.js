3f8e99da7a0a9eb389946cce8e2cc88e2e68cc75
shpendjahiri <44344752+shpendjahiri@users.noreply.github.com>
2020-05-30 23:28:55 +0200

    cart/controller

---
+++ ssh-shop-server/src/controllers/cart.js
@@ -0,0 +1,83 @@
+const {
+  CreateCart,
+  UpdateCart,
+  DeleteCart,
+  GetCart,
+} = require("../services/cart");
+
+const func = async (req, res) => {
+  await res.send("create");
+};
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+
+const create = async (req, res, next) => {
+  try {
+    //assign user_id to the user_id got by params
+    req.body.user_id = parseInt(req.params.user_id);
+    console.log({ ...req.body });
+
+    const cart = await CreateCart(req.body);
+    return res.status(200).json({ cart });
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+
+const update = async (req, res, next) => {
+  try {
+    const cart = await UpdateCart(
+      req.params.cart_id,
+      req.params.user_id,
+      req.body
+    );
+    return res.status(200).json({ cart });
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+const drop = async (req, res, next) => {
+  try {
+    const response = await DeleteCart(req.params.cart_id);
+    return res.status(200).json(response);
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {*} next
+ */
+const get = async (req, res, next) => {
+  try {
+    return res.status(200).json({ cart: await GetCart(req.params.cart_id) });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  create,
+  update,
+  drop,
+  func,
+  get,
+};
